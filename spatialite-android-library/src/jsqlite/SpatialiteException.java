d5b738b6621c47679ddaa244ac49287bd6d2b519
codeguru <codeguru@users.sourceforge.net>
2014-02-17 14:32:06 -0700

    Create `SpatialiteException`

---
+++ spatialite-android-library/src/jsqlite/SpatialiteException.java
@@ -0,0 +1,11 @@
+package jsqlite;
+
+public class SpatialiteException extends RuntimeException {
+
+    private static final long serialVersionUID = 3841251221139178164L;
+
+    public SpatialiteException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+}
